fc2be311cd9f31e726bb90faa7f4813254b6e09d
Mildred Manrique <mildred.manrique@outlook.es>
2021-11-12 11:35:41 -0600

    added models and migrations of news and more information

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,384 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <link type="text/css" rel="stylesheet" href="css/index.css">
+   
+
+
+    <title>CETI</title>
+</head>
+
+<body>
+<div id="app">
+    <!--Header section starts-->
+    <header>
+
+        <div id="menu-bar" class="fas fa-bars"></div>
+
+        <a href="#" class="logo"><span>C</span>ETI<span>.</span></a>
+
+        <nav class="navbar">
+            <a href="#home">Inicio</a>
+            <a href="#carreras">Carreras</a>
+            <a href="#convocatorias">Convocatorias</a>
+            <a href="#noticias">Noticias</a>
+            <a href="#contact">contáctanos</a>
+        </nav>
+
+        <div class="icons">
+            <i class="fas fa-search" id="search-btn"></i>
+            <i class="fas fa-user" id="login-btn"></i>
+        </div>
+
+        <form action="" class="search-bar-container">
+            <input type="search" id="search-bar" placeholder="search here...">
+            <label for="search-bar" class="fas fa-search"></label>
+        </form>
+    </header>
+    <!--Header section ends-->
+
+    <!--Login form container starts-->
+
+    <div class="login-form-container">
+
+        <i class="fas fa-times" id="form-close"></i>
+
+        <form action="">
+            <h3>login</h3>
+            <input type="email" class="box" placeholder="enter your email">
+            <input type="password" class="box" placeholder="enter your password">
+            <input type="submit" value="login now" class="btn">
+            <input type="checkbox" id="remember">
+            <label for="remember"> remember me</label>
+            <p>forget password? <a href="#">click here</a></p>
+            <p>don't have and accout? <a href="#">register now</a></p>
+        </form>
+    </div>
+    <!--Login form container end-->
+
+
+    <!--home section starts-->
+    <section class="home" id="home">
+
+        <div class="content">
+            <h3>Bienvenidos</h3>
+            <p>Centro de Estudios Técnicos Informáticos de Izamal.</p>
+            <a href="#" class="btn">Ver más</a>
+        </div>
+
+        <div class="controls">
+            <span class="img-btn active" data-src="img/untitled-25.jpg"></span>
+            <span class="img-btn" data-src="img/hackathon.jpg"></span>
+            <span class="img-btn" data-src="img/lenguas.jpg"></span>
+            <span class="img-btn" data-src="img/c.jpg"></span>
+            <span class="img-btn" data-src="img/fondo.jpg.jpg"></span>
+        </div>
+
+        <div class="img-container">
+            <img src="img/untitled-25.jpg" id="img-slider">
+        </div>
+    </section>
+    <!--home section ends-->
+
+    <!--Carreras section starts-->
+
+    <section class="carreras" id="carreras">
+
+        <h1 class="heading">
+            <span>c</span>
+            <span>a</span>
+            <span>r</span>
+            <span>r</span>
+            <span>e</span>
+            <span>r</span>
+            <span>a</span>
+            <span>s</span>
+        </h1>
+        <div class="box-container">
+
+            <div class="box">
+                <div class="images">
+                    <img src="img/hackathon.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>Computación</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed recusandae aspernatur aliquid,
+                        placeat
+                        sint possimus minima maiores quibusdam debitis nemo.</p>
+                    <a href="#" class="btn">leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="images">
+                    <img src="img/img_412114099.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>Cultura de belleza</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed recusandae aspernatur aliquid,
+                        placeat
+                        sint possimus minima maiores quibusdam debitis nemo.</p>
+                    <a href="#" class="btn">leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="images">
+                    <img src="img/shutterstock_170192996.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>Lenguas extranjeras</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed recusandae aspernatur aliquid,
+                        placeat
+                        sint possimus minima maiores quibusdam debitis nemo.</p>
+                    <a href="#" class="btn">leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="images">
+                    <img src="img/visadodeestudiantes_tolentinoabogados.jpg" alt="">
+                </div>
+                <div class="content">
+                    <h3>Bachillerato</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed recusandae aspernatur aliquid,
+                        placeat
+                        sint possimus minima maiores quibusdam debitis nemo.</p>
+                    <a href="#" class="btn">leer más</a>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    <!--Carreras section ends-->
+
+    <!--Convocatorias section starts-->
+
+    <section class="convocatorias" id="convocatorias">
+
+        <h1 class="heading">
+            <span>c</span>
+            <span>o</span>
+            <span>n</span>
+            <span>v</span>
+            <span>o</span>
+            <span>c</span>
+            <span>a</span>
+            <span>t</span>
+            <span>o</span>
+            <span>r</span>
+            <span>i</span>
+            <span>a</span>
+            <span>s</span>
+        </h1>
+
+        <div class="box-container">
+            <div class="box">
+                <i class="fas fa-comment-dollar"></i>
+                <h3>Becas</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam quis earum mollitia distinctio!
+                    Reprehenderit, voluptate?</p>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-people-arrows"></i>
+                <h3>Intercambio de escuelas</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam quis earum mollitia distinctio!
+                    Reprehenderit, voluptate?</p>
+            </div>
+
+            <div class="box">
+                <i class="fas fa-trophy"></i>
+                <h3>Concursos extracurriculares</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam quis earum mollitia distinctio!
+                    Reprehenderit, voluptate?</p>
+            </div>
+        </div>
+    </section>
+    <!--Convocatorias section ends-->
+
+    <!--Noticias section starts-->
+
+    <section class="noticias" id="noticias">
+
+        <h1 class="heading">
+            <span>n</span>
+            <span>o</span>
+            <span>t</span>
+            <span>i</span>
+            <span>c</span>
+            <span>i</span>
+            <span>a</span>
+            <span>s</span>
+        </h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="img/1.jpg" alt="">
+                <div class="content">
+                    <h3>Graduación</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nihil?</p>
+                    <a href="#" class="btn">Leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/2.jpg" alt="">
+                <div class="content">
+                    <h3>Hannal Pixan</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nihil?</p>
+                    <a href="#" class="btn">Leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/3.png" alt="">
+                <div class="content">
+                    <h3>Viaje al extranjero</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nihil?</p>
+                    <a href="#" class="btn">Leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/4.jpg" alt="">
+                <div class="content">
+                    <h3>maquillaje</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nihil?</p>
+                    <a href="#" class="btn">Leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/5.jpg" alt="">
+                <div class="content">
+                    <h3>Hackaton 2021</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nihil?</p>
+                    <a href="#" class="btn">Leer más</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="img/6.jpeg" alt="">
+                <div class="content">
+                    <h3>Ganadores de la beca</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nihil?</p>
+                    <a href="#" class="btn">Leer más</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Noticias section ends-->
+
+    <!--Contact section starts-->
+
+    <section class="contact" id="contact">
+
+        <h1 class="heading">
+            <span>c</span>
+            <span>o</span>
+            <span>n</span>
+            <span>t</span>
+            <span>á</span>
+            <span>c</span>
+            <span>t</span>
+            <span>a</span>
+            <span>n</span>
+            <span>o</span>
+            <span>s</span>
+        </h1>
+
+        <div class="row">
+
+            <form action="">
+                <input type="text" placeholder="name" class="box">
+                <input type="email" placeholder="email" class="box">
+                <input type="number" placeholder="number" class="box">
+                <textarea name="" class="box" placeholder="message" id="" cols="30" rows="10"></textarea>
+                <input type="submit" value="send message" class="btn">
+            </form>
+            <div class="image">
+                <img src="img/8.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="ubicacion">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d931.6185318879536!2d-89.02145827076332!3d20.933466551714595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f56a363022f209d%3A0xe47b0927ce440aca!2sTixkokob%20-%20Izamal%20323%2C%20Centro%2C%2097540%20Izamal%2C%20Yuc.!5e0!3m2!1ses-419!2smx!4v1636563905713!5m2!1ses-419!2smx"
+                width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </section>
+    <!--Contact section ends-->
+
+    <!--Footer section-->
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>Acerca de nosotros</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aspernatur non fugit nesciunt sunt! Laudantium eius provident veniam, voluptas eligendi similique tempora, quisquam reprehenderit consectetur esse ipsa qui nihil eos soluta? Eum error labore tempora!</p>
+            </div>
+
+
+            <div class="box">
+                <h3>misión</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, delectus! Debitis asperiores fugiat dolores voluptatem, est non rem? Odio rerum vitae voluptate enim dignissimos earum commodi quis ex aperiam neque!</p>
+            </div>
+
+            <div class="box">
+                <h3>visión</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, quibusdam ut? Quod, dolore
+                    deserunt! Optio excepturi quasi delectus vitae adipisci temporibus facilis pariatur architecto
+                    officiis?</p>
+            </div>
+
+            <div class="box">
+                <h3>Redes sociales</h3>
+                <a href="#">facebook</a>
+                <a href="#">instagram</a>
+                <a href="#">twitter</a>
+                <a href="#">youtube</a>
+            </div>
+        </div>
+
+        <h1 class="credit">created by <span>Mildred Manrique</span></h1>
+    </section>
+
+
+
+
+
+<example-component></example-component>
+
+
+</div>
+
+
+
+
+
+
+
+    <!--custom js file link-->
+
+    <script src="/js/app.js"></script>
+    <script src="/js/script.js"></script>
+   
+</body>
+
+</html>
